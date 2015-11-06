cd4d4c9268314dab77deb072ec753988aaebc0dd
onyzhin <o.nyzhin@gmail.com>
2015-11-06 13:45:52 +0200

    tour block done

--- js/common.js
+++ js/common.js
@@ -1,4 +1,20 @@
+function star(rating){
+	$(rating).each(function(){
+		var rating = $(this).data('rating');
+		var maxrating = 5;
+		for (var i = 0; i < maxrating; i++){
+			console.log(i);
+			$(this).append('<span></span>');
+			if (i<rating)
+				$(this).find('span').eq(i).addClass('active');
+		}
+	})
+}
+
 $(function(){
+
+	star('.tour-rating');
+
 	$('.fancybox').fancybox({
 		helpers: {
 			overlay: {
