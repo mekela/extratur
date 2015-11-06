0272db71a9cd1aa6b3b6d4ea020459951e8c0402
alex <sania_free@i.ua>
2015-11-06 15:56:08 +0200

    all my blocks

--- js/common.js
+++ js/common.js
@@ -8,4 +8,12 @@ $(function(){
 	});
 	
 	$('input[placeholder], textarea[placeholder]').placeholder();
+
+	 $('.our_workers ul').bxSlider({
+	    slideWidth: 176,
+	    minSlides: 4,
+	    maxSlides: 4,
+	    pager: false,
+	    slideMargin: 64
+	  });
 });
